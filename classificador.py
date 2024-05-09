9365065a760a9caec723350b9039b18a0e5c397b
alinelimx <alinelima24@alu.ufc.br>
2024-05-09 09:53:45 -0300

    classificador de nivel de heroi

--- classificador.py
+++ classificador.py
@@ -18,39 +18,25 @@
 #Ao final deve se exibir uma mensagem:
 #"O Herói de nome **{nome}** está no nível de **{nivel}**"
 
-nomeHeroi = input("nome do heroi: ")
-XP = int(input("XP: "))
+for i in range (7):
+    nomeHeroi = input("nome do heroi: ")
+    XP = int(input("XP: "))
 
-for i in range(7):
     if XP < 1000:
         nivel = "Ferro"
-        print("O Herói de nome", nomeHeroi, "está no nível de", nivel)
-        XP += 1000
-    elif XP > 1001 and XP < 2000:
+    elif XP <= 2000:
         nivel = "Bronze"
-        print("O Herói de nome", nomeHeroi, "está no nível de", nivel)
-        XP += 1000
-    elif XP > 2001 and XP < 5000:
+    elif XP <= 5000:
         nivel = "Prata"
-        print("O Herói de nome", nomeHeroi, "está no nível de", nivel)
-        XP += 3000
-    elif XP > 5001 and XP < 7000:
+    elif XP <= 7000:
         nivel = "Ouro"
-        print("O Herói de nome", nomeHeroi, "está no nível de", nivel)
-        XP += 2000
-    elif XP > 7001 and XP < 8000:
+    elif XP <= 8000:
         nivel = "Platina"
-        print("O Herói de nome", nomeHeroi, "está no nível de", nivel)
-        XP += 1000
-    elif XP > 8001 and XP < 9000:
+    elif XP <= 9000:
         nivel = "Ascendente"
-        print("O Herói de nome", nomeHeroi, "está no nível de", nivel)
-        XP += 1000
-    elif XP > 9001 and XP < 10000:
+    elif XP <= 10000:
         nivel = "Imortal"
-        print("O Herói de nome", nomeHeroi, "está no nível de", nivel)
-        XP += 1000
-    elif XP >= 10001:
+    else:
         nivel = "Radiante"
-        print("O Herói de nome", nomeHeroi, "está no nível de", nivel)
 
+    print("O Herói de nome", nomeHeroi, "está no nível de", nivel)
